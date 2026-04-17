38a78bcec2d55dfeff25c588290629dcd5867f6b
stathiw <stathi.weir@gmail.com>
2026-04-17 10:35:02 -0700

    Remove BAG2VID_SRC env var, now mount /home/stathi for full access

--- bag2vid/scripts/bag2vid.sh
+++ bag2vid/scripts/bag2vid.sh
@@ -3,15 +3,15 @@
 # Variables
 BAG2VID_IMAGE="stathiw/bag2vid:latest"
 CONTAINER_NAME="bag2vid"
-BAG2VID_SRC="/home/${USER}/rosbags"  # Replace with the path to your ROS bags
 DISPLAY_VAR=${DISPLAY}
 
 # Run the Docker container
 docker run -it --rm \
   --name ${CONTAINER_NAME} \
   --env DISPLAY=${DISPLAY_VAR} \
+  --env HOME=${HOME} \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
-  --volume ${BAG2VID_SRC}:/home/ubuntu/rosbags \
+  --volume ${HOME}:${HOME} \
   --network host \
   --privileged \
   ${BAG2VID_IMAGE} \
